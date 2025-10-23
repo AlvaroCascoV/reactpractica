5eb5c0370a8eef91be73050dd94dabf08b63724a
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-23 14:03:46 +0200

    final

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -4,6 +4,9 @@ import Global from '../Global'
 import axios from 'axios';
 
 export default class Buscador extends Component {
+    cajaNombre = React.createRef();
+    selectEquipo = React.createRef();
+
     url = Global.urlEquipos;
     getEquipos = () => {
         let request = "api/Equipos"
@@ -14,31 +17,69 @@ export default class Buscador extends Component {
             })
         })
     }
+
     state = {
         equipos: [],
+        busqueda: "",
+        idequiposeleccionado: 0,
+        mensaje: null
     }
 
     componentDidMount = () => {
         this.getEquipos();
     }
+
+    seleccionarEquipo = (event) => {
+        event.preventDefault();
+        let id = this.selectEquipo.current.value
+        console.log("equipo " + id)
+        this.setState({
+            idequiposeleccionado: id
+        })
+    }
+
+    buscarNombre = (event) => {
+        event.preventDefault();
+        let stringBusqueda = this.cajaNombre.current.value
+        if (stringBusqueda) {
+            console.log("buscar " + stringBusqueda)
+            this.setState({
+                busqueda: stringBusqueda,
+                mensaje: null,
+            })
+        } else {
+            this.setState({
+                mensaje: "Escribe para buscar"
+            })
+        }
+    }
+
     render() {
         return (<div>
             <h1>Buscador jugadores</h1>
             <form>
                 <label>Nombre de jugador</label>
                 <input type='text' ref={this.cajaNombre} className='form-control' />
-                <button className='btn btn-danger'>Buscar jugadores nombre</button><hr />
+                {
+                    this.state.mensaje && (
+                        <h2 style={{ color: "fuchsia" }}>{this.state.mensaje}</h2>
+                    )
+                }
+                <button className='btn btn-danger' onClick={this.buscarNombre}>Buscar jugadores nombre</button>
+                <hr />
+
                 <label>Seleccione equipo</label>
-                <select className='form-control'>
+                <select className='form-control' ref={this.selectEquipo}>
                     {
                         this.state.equipos.map((equipo, index) => {
-                            return (<option key={index}>{equipo.nombre}</option>)
+                            return (<option key={index} value={equipo.idEquipo}>{equipo.nombre}</option>)
                         })
                     }
                 </select>
-                <button className='btn btn-info'>Buscar jugadores equipo</button>
+                <button className='btn btn-info' onClick={this.seleccionarEquipo}>Buscar jugadores equipo</button>
                 <hr />
-                <Jugadores />
+
+                < Jugadores idEquipo={this.state.idequiposeleccionado} stringBusqueda={this.state.busqueda} />
             </form>
         </div>)
     }
--- src/components/Jugadores.jsx
+++ src/components/Jugadores.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from 'react'
+import Global from '../Global'
+import axios from 'axios'
 
 export default class Jugadores extends Component {
+    url = Global.urlEquipos
+
+    findJugadoresEquipo = () => {
+        let request = "api/Jugadores/JugadoresEquipos/" + this.props.idEquipo
+        axios.get(this.url + request).then(response => {
+            console.log("jugadores de " + this.props.idEquipo)
+            this.setState({
+                jugadores: response.data
+            })
+        })
+    }
+
+    findJugadoresNombre = () => {
+        let request = "api/Jugadores/BuscarJugadores/" + this.props.stringBusqueda;
+        axios.get(this.url + request).then(response => {
+            console.log("buscado de " + this.props.stringBusqueda)
+            this.setState({
+                jugadores: response.data
+            })
+        })
+    }
+
+    componentDidUpdate = (oldprops) => {
+        if (oldprops.idEquipo !== this.props.idEquipo) {
+            this.findJugadoresEquipo();
+        } else if (oldprops.stringBusqueda !== this.props.stringBusqueda) {
+            this.findJugadoresNombre();
+        }
+    }
+
+    componentDidMount = () => {
+        this.findJugadoresEquipo();
+    }
+
+    state = {
+        jugadores: []
+    }
     render() {
         return (<div>
-            <h1>Jugadores component</h1>
+            <h1 style={{ color: "red" }}>Jugadores component</h1>
             <table className='table table-info'>
                 <thead>
                     <tr>
@@ -15,10 +54,28 @@ export default class Jugadores extends Component {
                 </thead>
                 <tbody>
                     {
-                        //cargar jugadores
+                        this.state.jugadores.map((jugador, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td><img src={jugador.imagen} alt='imagenjugador' style={{ height: "50px" }} /></td>
+                                    <td>{jugador.nombre}</td>
+                                    <td>{jugador.posicion}</td>
+                                    <td>{jugador.pais}</td>
+                                </tr>
+                            )
+                        })
                     }
                 </tbody>
             </table>
         </div>)
     }
 }
+// {
+//     "idJugador": 1,
+//     "nombre": "Antoine Griezmann",
+//     "posicion": "Delantero",
+//     "imagen": "https://img.a.transfermarkt.technology/portrait/header/125781-1719928503.jpg",
+//     "fechaNacimiento": "21/03/1991",
+//     "pais": "Francia",
+//     "idEquipo": 1
+//   },
